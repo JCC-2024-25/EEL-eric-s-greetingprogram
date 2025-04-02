ad037dd288ff7fa662b59f906bcd2e98637778dc
book-lover-man <eric.shvidky@gmail.com>
2025-04-02 17:28:36 +0000

    rizz

--- calculator.py
+++ calculator.py
@@ -1,5 +1,18 @@
-number1 = input("What is your first number?: ")
-equation = input("What is your equation?: ")
-number2 = input("What is your second number")
-answer = number1; equation; number2
-print(f"{answer}")
+while True:
+    number1 = float(input("Enter the first number: "))
+    equation = input("Enter the equation (+,/,-,*):")
+    number2 = float(input("Enter the second number: "))
+    ready = input("Press enter to get the answer: ")
+    ready = "="
+    if equation == "+" and ready == "=":
+       print(f"The answer is {number1 + number2}")
+    if equation == "-" and ready == "=":
+       print(f"The answer is {number1 - number2}")
+    if equation == "/" and ready == "=":
+       print(f"The answer is {number1 / number2}")
+    if equation == "*" and ready == "=":
+       print(f"The answer is {number1 * number2}")
+    repeat = input("Do you want to do another calculation? (y/n):")
+    if repeat.lower() == "n":
+        print("Goodbye!")
+        break
